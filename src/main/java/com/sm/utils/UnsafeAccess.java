196268cb1318d2560a599a09ec51ebdf6faad667
Suvankar Mazumder <smazumder@gmail.com>
2016-12-22 15:44:10 -0500

    Update UnsafeAccess.java

--- src/main/java/com/sm/utils/UnsafeAccess.java
+++ src/main/java/com/sm/utils/UnsafeAccess.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.bb.utils;
+package com.sm.utils;
 
 import java.lang.reflect.Field;
 import java.security.AccessController;
